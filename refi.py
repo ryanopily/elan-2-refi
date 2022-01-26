a6e8cc0aaa2c0edc581830607a9bb81e362aee8a
ryanopily <ryansmarcil@gmail.com>
2022-01-26 16:56:55 -0500

    Cleaning up refi.py / Documenting

--- refi.py
+++ refi.py
@@ -2,29 +2,26 @@ import os
 import shutil
 import argparse
 
+import pympi
 from zipfile import ZipFile
 
-from refi_common import parentFolder, stripPath, mkdirs
-
-import pympi
 import elan_to_refi
+from refi_common import parentFolder, stripPath, mkdirs
   
-def invoke(projectDir, outputName):
+def invoke(projectDir, outputFile):
     
     elan_name = None
     elan_file = None
     pfsx_file = None
-    transcript_file = None
-
     video_files = []
     audio_files = []
-    
-    qdpx_output = os.getcwd()
+
     project_files = os.listdir(projectDir) 
     os.chdir(projectDir)
 
-    # Search for ELAN project and preferences files
+    # Search for project files
     for file in project_files:
+
         if os.path.isfile(file):
             name, extension = os.path.splitext(file)
             
@@ -34,25 +31,25 @@ def invoke(projectDir, outputName):
 
             elif extension == '.pfsx':
                 pfsx_file = file
-        
+
+           
     if elan_file is None:
         raise Exception(f"No ELAN (EAF) project file found!\n Found: {project_files}")
 
+    # The PFSX file is needed to get tiers to include in the generated transcript 
     if pfsx_file is None:
         raise Exception(f"No ELAN (PFSX) preferences file found!\n Found: {project_files}")
 
-    QDE = elan_to_refi.convert(elan_file, pfsx_file)
-    project = pympi.Elan.Eaf(elan_file)
+    elan_project = pympi.Elan.Eaf(elan_file)
+    # XML Element Tree representing QDPX (QDE) Project file
+    qde_tree = elan_to_refi.convert(elan_file, pfsx_file)
 
-    transcript_file = name + ".txt"
-
-
-    # Search for video file
-    for media in project.media_descriptors:
+    # Search for media files
+    for media in elan_project.media_descriptors:
         
         mime = media['MIME_TYPE']
         
-        def collect_media(target):
+        def collect_media(media, target):
             absolute = media['MEDIA_URL']
             relative = media['RELATIVE_MEDIA_URL']
            
@@ -66,41 +63,38 @@ def invoke(projectDir, outputName):
                 raise Exception(f"Media file {absolute} or {relative} couldn't be found!")           
         
         if mime == 'audio/x-wav':
-            collect_media(audio_files)
+            collect_media(media, audio_files)
         
         if mime == 'video/mp4':     
-            collect_media(video_files)
+            collect_media(media, video_files)
 
-    # Create folder for QDPX file
-    parent = 'elan2refi'
-    qde_file = 'project.qde'
-
-    sources_file = os.path.join(parent, 'sources')
-    mkdirs(sources_file)
-
-    shutil.copyfile(transcript_file, os.path.join(sources_file, transcript_file)) 
-    
-    os.chdir(parent)
-    QDE.write(qde_file,encoding='utf-8',xml_declaration=True)
-        
-    if outputName is None:
+    # outputFile will be named after the elan project and placed in the same directory as it by default
+    if outputFile is None:
         outputName, extension = stripPath(elan_file)
-        
-    extension = '.qdpx'
+        outputFile = os.path.join(os.getcwd(), outputName + '.qdpx')
 
-    with ZipFile(os.path.join(qdpx_output, outputName + extension), 'w') as archive:
+    # Build folder to compress into QDPX archive
+    archive_target = 'elan2refi'
+    sources_file = os.path.join(archive_target, 'sources')
+    mkdirs(sources_file)
+    transcript_file = elan_name + '_transcript.txt'
+    shutil.copyfile(transcript_file, os.path.join(sources_file, transcript_file)) 
+    os.chdir(archive_target)
+    qde_file = 'project.qde'
+    qde_tree.write(qde_file,encoding='utf-8',xml_declaration=True)
+
+    with ZipFile(outputFile, 'w') as archive:
         archive.write(qde_file)
-        sources_file = 'sources'
 
-        for file in os.listdir(sources_file):
-            file = os.path.join(sources_file, file)
+        for file in os.listdir('sources'):
+            file = os.path.join('sources', file)
             archive.write(file)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', metavar='file', type=str, default=os.getcwd(), help='ELAN (EAF) project file OR directory')
-    parser.add_argument('-o', metavar='output', type=str, default=None, help='Name of output file (QDPX)')
+    parser.add_argument('-f', metavar='file', type=str, default=os.getcwd(), help='EAF project file OR directory')
+    parser.add_argument('-o', metavar='output', type=str, default=None, help='Path of qdpx output file')
 
     args = parser.parse_args()
     projectDir = parentFolder(args.f) if os.path.isfile(args.f) else args.f
